270dd0de372e268a2c47a283a022de89a9e3ac25
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-07-06 13:58:38 +0530

    Add validation schema for member.ts

--- src/dto/member.ts
+++ src/dto/member.ts
@@ -1,6 +1,15 @@
+import Joi from "joi";
+
 export class Member{
 
     constructor(public _id:string, public name:string, public address:string,
                 public contact:string) {
     }
+
+    public static readonly SCHEMA = Joi.object({
+        _id: Joi.string().required().regex(/^\d{9}[Vv]$/),
+        name: Joi.string().required().regex(/[A-Za-z ]+/),
+        address: Joi.string().min(3).required(),
+        contact: Joi.string().required().regex(/^\d{3}-\d{7}$/)
+    });
 }
