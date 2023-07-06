9389c02ebd94bcdf319ea3b79ba29c9cb0d10df3
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-07-06 13:45:27 +0530

    Make some changes main.ts and member-rest-controller.ts classes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
-import express from 'express';
 import env from 'dotenv';
+import {Db, MongoClient} from "mongodb";
+import express, {json} from 'express';
 import {router as MemberRouter} from './api/member-rest-controller';
 
 env.config();
-const app = express();
-app.use("/api/v1/members", MemberRouter);
+export let datasource: Db;
 
-app.listen(process.env.APP_PORT, ()=>
-    console.log(`Server has been started on ${process.env.APP_PORT}`));
+async function main(){
+    const mongoClient = new MongoClient(process.env.APP_DB_URL!);
+    await mongoClient.connect();
+    console.log("Successfully connected with the MonogoDB Server");
+    datasource =  mongoClient.db(process.env.APP_DB_NAME)
+}
+
+main().then(value => {
+    const app = express();
+
+    app.use(json());
+    app.use("/api/v1/members", MemberRouter);
+
+    app.listen(process.env.APP_PORT,
+        ()=> console.log(`Server has been created at ${process.env.APP_PORT}`))
+}).catch(console.error);
